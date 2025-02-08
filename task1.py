bd7621c001c15537f166c5f2fd08c62f9b5cd45a
yevhenii-bezeha <bezegajenja0305@gmail.com>
2025-02-08 14:00:53 +0100

    HW

---
+++ task1.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def min_cost_to_connect_cables(cables):
+    heapq.heapify(cables)
+    total_cost = 0
+
+    while len(cables) > 1:
+        first = heapq.heappop(cables)
+        second = heapq.heappop(cables)
+        cost = first + second
+        total_cost += cost
+        heapq.heappush(cables, cost)
+
+    return total_cost
+
+
+test = [4, 3, 2, 6, 1]
+print(min_cost_to_connect_cables(test))
